4c51cbc653573f85a456cb1720067290fab741d5
dikshakrishna <43814593+dikshakrishna@users.noreply.github.com>
2020-08-28 21:23:06 +0530

    Update getImage.py
    Here is a simple code to capture an image through a webcam using OpenCV and saving it as a black & white image.

--- getImage.py
+++ getImage.py
@@ -6,13 +6,13 @@ def getImage():
 
     while(True):
         rect, img = image.read()
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        img = cv2.resize(img, (200, 200))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #change the colour to grayscale
+        img = cv2.resize(img, (200, 200)) #defines the dimension as 200*200
         cv2.imshow('img1', img)  # display the captured image
 
         if cv2.waitKey(1) & 0xFF == ord('y'):  # save on pressing 'y'
 
-            cv2.imwrite('Images\c1.png', img)
+            cv2.imwrite('image.png', img) #saves to the folder as image.png
             cv2.destroyAllWindows()
             break
 
